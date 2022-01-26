5f6854837f67709bde4d8c16f0172a0bb89a8364
loop <34685468+ArvanBishwas@users.noreply.github.com>
2022-01-26 19:38:18 +0600

    upload part1

---
+++ part1- keyboard controlled RC car/computer_controller/keyboard_client.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pygame
+import socket
+import struct
+import sys
+from pygame.locals import *
+
+from settings import *
+
+
+class KeyboardClient(object):
+    def __init__(self, host, port):
+        pygame.init()
+        pygame.display.set_mode((500, 500))
+        pygame.display.set_caption('Pygame Keyboard Monitoring')
+        # Keyboard command client, sends the keyboard input commands to the Raspberry Pi
+        self.keyboard_client_socket = socket.socket()
+        self.keyboard_client_socket.connect((host, port))
+        print("Keyboard Socket Connected!")
+
+    def monitor(self):
+        """Monitor keyboard events and notify the server"""
+        try:
+            while True:
+                for event in pygame.event.get():
+                    if event.type == KEYDOWN:
+                        key = pygame.key.get_pressed()
+                        if key[pygame.K_UP] and key[pygame.K_LEFT]:
+                            print("Forward Left")
+                            self.keyboard_client_socket.send(struct.pack('<I', FORWARD | LEFT))
+                        elif key[pygame.K_UP] and key[pygame.K_RIGHT]:
+                            print("Forward Right")
+                            self.keyboard_client_socket.send(struct.pack('<I', FORWARD | RIGHT))
+                        elif key[pygame.K_DOWN] and key[pygame.K_LEFT]:
+                            print("Backward Left")
+                            self.keyboard_client_socket.send(struct.pack('<I', BACKWARD | LEFT))
+                        elif key[pygame.K_DOWN] and key[pygame.K_RIGHT]:
+                            print("Backward Right")
+                            self.keyboard_client_socket.send(struct.pack('<I', BACKWARD | RIGHT))
+                        elif key[pygame.K_LEFT]:
+                            print("Left")
+                            self.keyboard_client_socket.send(struct.pack('<I', LEFT))
+                        elif key[pygame.K_RIGHT]:
+                            print("Right")
+                            self.keyboard_client_socket.send(struct.pack('<I', RIGHT))
+                        elif key[pygame.K_UP]:
+                            print("Forward")
+                            self.keyboard_client_socket.send(struct.pack('<I', FORWARD))
+                        elif key[pygame.K_DOWN]:
+                            print("Backward")
+                            self.keyboard_client_socket.send(struct.pack('<I', BACKWARD))
+                        elif key[K_ESCAPE]:
+                            print("Shutdown and Exit")
+                            self.keyboard_client_socket.send(struct.pack('<I', STOP))
+                            self.keyboard_client_socket.send(struct.pack('<I', SHUTDOWN))
+                            sys.exit(0)
+                        else:
+                            print("Undefined Option")
+                    elif event.type == pygame.KEYUP:
+                        print("Stop")
+                        self.keyboard_client_socket.send(struct.pack('<I', STOP))
+
+        finally:
+            print("Connection Closed!")
+            self.keyboard_client_socket.close()
+
+
+if __name__ == "__main__":
+    keyboard = KeyboardClient(host=PI_HOST, port=KEYBOARD_PORT)
+    keyboard.monitor()
---
+++ part1- keyboard controlled RC car/pi-RCcar/setting.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Raspberry Pi pin (BCM number) to which the DC motor is connected
+LEFT_PIN_A = 17
+LEFT_PIN_B = 22
+LEFT_PWM = 0
+RIGHT_PIN_A = 23
+RIGHT_PIN_B = 24
+RIGHT_PWM = 27
+
+# speed regulation of the car
+GEARS = [0, 1, 2, 3, 4]
+
+# Define the operation command
+STOP     = 0b0000
+LEFT     = 0b0001
+RIGHT    = 0b0010
+FORWARD  = 0b0100
+BACKWARD = 0b1000
+SHUTDOWN = 0b1111
+
+SERVER_HOST = "0.0.0.0"
+PI_HOST = "192.168.0.7"
+COMPUTER_HOST = "192.168.0.11"
+VIDEO_STREAMING_PORT = 8000
+KEYBOARD_PORT = 8001
+
+# Set the size of the image captured by the camera
+RESOLUTION = (640, 480)
